3ea27553577d2644f375b645b7e685e8a11f48aa
MH Hamza <mhamza2557@gmail.com>
2018-03-03 16:01:34 +0500

    Chapter 1

---
+++ Chapter 1/WelcomeWithThreeMessages.java
@@ -0,0 +1,7 @@
+public class WelcomeWithThreeMessages {
+	public static void main(String[] args) {
+		System.out.println("programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+}
