8ddf216b628c1c79b49a7ac61417e44e38c5a82c
MH Hamza <mhamza2551@gmail.com>
2019-01-02 17:46:05 +0500

    Delete WelcomeWithThreeMessages.java

--- Chapter 1/WelcomeWithThreeMessages.java
+++
@@ -1,7 +0,0 @@
-public class WelcomeWithThreeMessages {
-	public static void main(String[] args) {
-		System.out.println("programming is fun!");
-		System.out.println("Fundamentals First");
-		System.out.println("Problem Driven");
-	}
-}
