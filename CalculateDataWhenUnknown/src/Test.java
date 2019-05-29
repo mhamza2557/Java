110b467f203d99cd5ab2bb2418e8ad78dfc4b0b9
mhamza2557 <mhamza2551@gmail.com>
2019-05-29 17:57:03 +0500

    Del

--- CalculateDataWhenUnknown/src/Test.java
+++
@@ -1,7 +0,0 @@
-/*public class Test {
-    public static void main(String[] args) {
-        CalculateData calculateData = new CalculateData();
-        calculateData.CalculateDataWhenUnknown(20,250);
-        calculateData.displayData();
-    }
-}*/
--- NADRA/src/TypeService.java
+++
@@ -1,4 +0,0 @@
-public interface TypeService {
-    int typeService();
-    void printDetails();
-}
